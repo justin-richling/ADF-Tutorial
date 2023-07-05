d8c80c3709a49e5d0a1266eb277e1b829c6921b9
justin-richling <richling@ucar.edu>
2023-07-05 13:02:22 -0600

    Delete files

--- _build/jupyter_execute/notebooks/intro/layout/env.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Conda Env
-
-# Here is a collection of necessary packages to run the ADF built into a conda environment.
-# 
-# 
--- _build/jupyter_execute/notebooks/intro/layout/scripts.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Scripts
-# 
-# Collection of scripts for climotology and time series file creation, regridding, anaylsis, and plotting
-
-# 
